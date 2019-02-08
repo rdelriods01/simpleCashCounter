8e6ef51c9133cadb4a73eb3984038f018203ef11
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-08 02:12:15 -0600

    add css and operational functions, TODO: Responsive

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 sumar = () => {
-    console.log("Si jala el boton");
     const quin = document.getElementById("to500").value * 500
     const dosc = document.getElementById("to200").value * 200
     const cien = document.getElementById("to100").value * 100
     const cinc = document.getElementById("to50").value * 50
     const vein = document.getElementById("to20").value * 20
-
     const total = quin + dosc + cien + cinc + vein;
-
     document.getElementById("total").innerHTML = total.toString();
 }
 
@@ -18,4 +15,11 @@ reset = () => {
     document.getElementById("to50").value = 0
     document.getElementById("to20").value = 0
     document.getElementById("total").innerHTML = "0";
+}
+handleInput = (value) => {
+    if (value.length < 2) {
+        return 0
+    } else {
+        return parseInt(value.replace(/[^0-9]/g, ''), 10)
+    }
 }
